7cd8037e18413b6ed8159d94275e143793cce7e7
wxy <shiningrise@gmail.com>
2016-08-04 20:40:14 +0800

    111

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#sudo wget linuxdot.net/down/jexus-5.8.2-x64.tar.gz
 sudo docker build -t jexus:5.8.2 .
+
--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 
 sudo docker login --username=shiningrise@aliyun.com registry.cn-hangzhou.aliyuncs.com
 
-sudo docker tag [ImageId] registry.cn-hangzhou.aliyuncs.com/shiningrise/jexus:5.8.2
+sudo docker tag 73cad642815b registry.cn-hangzhou.aliyuncs.com/shiningrise/jexus:5.8.2
 sudo docker push registry.cn-hangzhou.aliyuncs.com/shiningrise/jexus:5.8.2
